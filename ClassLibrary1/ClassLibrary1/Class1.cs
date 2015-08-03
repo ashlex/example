2bf62dc9a848cb05dbaa977adc3322aa0e2f4ae3
ashlex <angeldust.ru@gmail.com>
2015-08-03 08:46:29 +0300

    first commit

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1.Properties;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+	    private Class1()
+	    {
+	    }
+
+	    /// <summary>
+		/// This calc area of retangle.
+		/// </summary>
+		/// <exception cref="ArgumentNullException">If any of parameters is less than zero</exception>
+		/// <returns>Returns value of area of a rectangle</returns>
+		public static double GetAreaOfRectangle(double a, double b)
+	    {
+			if(a<0||b<0)throw new ArgumentOutOfRangeException(Resources.less_than_zero);
+		    return a*b;
+	    }
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/Class1Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+	[TestClass()]
+	public class Class1Tests
+	{
+		[TestMethod()]
+		public void GetAreaOfRectangleTest()
+		{
+			try
+			{
+				Class1.GetAreaOfRectangle(-1, 1);
+				Assert.Fail();
+			}
+			catch (ArgumentOutOfRangeException)
+			{ }
+			try
+			{
+				Class1.GetAreaOfRectangle(1, -1);
+				Assert.Fail();
+			}
+			catch (ArgumentOutOfRangeException)
+			{ }
+			Assert.AreEqual(6, Class1.GetAreaOfRectangle(2, 3));
+			Assert.AreEqual((double)6.9, Class1.GetAreaOfRectangle(2.3, 3),0.1);
+		}
+	}
+}
